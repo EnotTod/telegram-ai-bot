4e9e44e1c5fdecf255d7a5bfb37f039e150503d6
EnotTod <37248015+EnotTod@users.noreply.github.com>
2025-03-15 13:53:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,3 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-if os.environ.get("RUN_MAIN_SCRIPT"):
-    # Запуск основного скрипта
-    import main
-else:
-    # Запуск другого скрипта
-    # import other_script
